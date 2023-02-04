fbef2a9af3f07d57c0bd4204d4bb9b0445c33a6c
Rifat <106447308+rifatbinsaleh@users.noreply.github.com>
2023-02-04 22:15:26 +0600

    Add files via upload

---
+++ HW-1/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    int x=5;
+    void sum()
+    {
+        x=x+5;
+        System.out.println(x);
+    }
+    public static void main( String[] args){
+        Student obj = new Student();
+        obj.Name = "ABC";
+        obj.Age = 80;
+        obj.CGPA =3.5f;
+        obj.display();
+
+    }
+}
---
+++ HW-1/src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    String Name;
+    int Age;
+    float CGPA;
+
+    void display() {
+        System.out.println(Name);
+        System.out.println(Age);
+        System.out.println(CGPA);
+    }
+
+}
