637c6b399e912f0fe4907fd9bf6df33bbb5e7e23
Rifat <106447308+rifatbinsaleh@users.noreply.github.com>
2023-02-12 12:52:55 +0600

    Add files via upload

---
+++ lab-5/src/Employee1.java
@@ -0,0 +1,16 @@
+public class Employee1 {
+    int id;
+    String name;
+
+    Employee1(int i,String n){
+        this.id=i;
+        this.name=n;
+    }
+
+    void info()
+    {
+        System.out.println("ID:"+id);
+        System.out.println("Name:"+name);
+    }
+
+}
---
+++ lab-5/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Employee1 em1 = new Employee1(4846, "Rifat");
+        em1.info();
+    }
+}
